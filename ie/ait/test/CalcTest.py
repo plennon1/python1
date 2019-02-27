dbb1a3db0d71b226f8552177db74de0480f3eb1c
Paul Lennon <lennon.paul@gmail.com>
2019-02-27 15:49:25 +0000

    initial files added

---
+++ ie/ait/test/CalcTest.py
@@ -0,0 +1,12 @@
+import unittest
+from ie.ait.project1.Calc import Calc
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        myCalc = Calc()
+        myCalc.myPrint()
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
